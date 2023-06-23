278262372036e48adfd33b5418ec96452053aca9
bharani <bharanikumar070@gmail.com>
2023-06-23 16:18:01 +0530

    Blog with Custom Dashboard

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 #from django.http import HttpResponse
-from blogs.models import Blog, Category
+from blogs.models import Blog, Category, Comment
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 
 def posts_by_category(request, category_id):
     posts = Blog.objects.filter(status='Published', category=category_id)
@@ -17,8 +18,21 @@ def posts_by_category(request, category_id):
 
 def blogs(request, post_slug):
     single_blog = get_object_or_404(Blog, slug=post_slug, status="Published")
+    if request.method =='POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    # comments
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
     context = {
         'single_blog' : single_blog,
+        'comments' : comments,
+        'comment_count' : comment_count
     } 
     return render(request, 'blogs.html', context)
 
